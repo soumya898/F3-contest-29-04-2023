ee41f44b748a8eb264d63513827a339bee752c83
Soumya Ranjan Nayak <soumyaranjannayak0140@gmail.com>
2023-04-29 23:21:31 +0530

    add js

---
+++ script.js
@@ -0,0 +1,21 @@
+// console.log(" all are Good");
+
+
+// Store Input Values  by variables.
+
+let Name=document.getElementById("name");
+let Email=document.getElementById('email');
+let Password=document.getElementById("password");
+let Confirmpassword=document.getElementById("confirmpassword");
+
+
+
+ let successEl = document.getElementById('success');
+ let errorEl = document.getElementById('error');
+ let passwordMismatchedEl = document.getElementById('passwordMismatched');
+ let invalidemailEl = document.getElementById('invalidEmail');
+ let submitBtn = document.getElementById('btn');
+
+
+
+ 
